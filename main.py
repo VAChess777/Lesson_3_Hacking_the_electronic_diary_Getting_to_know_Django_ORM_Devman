d8fdd53e0ede5c00b72a8757c508faf2ff8cefaa
VAChess777 <109584765+VAChess777@users.noreply.github.com>
2022-12-03 15:45:17 +0300

    update_1_main

--- main.py
+++ main.py
@@ -12,24 +12,12 @@ from datacenter.models import (
 )
 
 
-commendations = [
-    'Молодец!', 'Отлично!', 'Хорошо!', 'Гораздо лучше, чем я ожидал!', 'Ты меня приятно удивил!',
-    'Великолепно!', 'Прекрасно!', 'Ты меня очень обрадовал!', 'Именно этого я давно ждал от тебя!',
-    'Сказано здорово – просто и ясно!', 'Ты, как всегда, точен!', 'Очень хороший ответ!', 'Талантливо!',
-    'Ты сегодня прыгнул выше головы!', 'Я поражен!', 'Уже существенно лучше!', 'Потрясающе!',
-    'Замечательно!', 'Прекрасное начало!', 'Так держать!', 'Ты на верном пути!', 'Здорово!',
-    'Это как раз то, что нужно!', 'Я тобой горжусь!', 'С каждым разом у тебя получается всё лучше!',
-    'Мы с тобой не зря поработали!', 'Я вижу, как ты стараешься!', 'Ты растешь над собой!',
-    'Ты многое сделал, я это вижу!', 'Теперь у тебя точно все получится!'
-    ]
-
-
 def fix_marks(schoolkid):
-    bad_marks = Mark.objects.filter(schoolkid=schoolkid, points__in=[1, 2, 3]).all()
-    for mark in bad_marks:
-        mark.points = 5
-        mark.save()
-    return bad_marks.count()
+    fix_bad_marks = Mark.objects.filter(
+        schoolkid=schoolkid,
+        points__in=[1, 2, 3]
+    ).update(points=5)
+    return fix_bad_marks
 
 
 def remove_chastisements(schoolkid):
@@ -62,23 +50,64 @@ def create_parser():
     parser.add_argument(
         'schoolkid_and_subject',
         help=
-        'Enter the command in console: $ python main.py schoolkid_and_subject {Фролов Иван Математика}. '
-        'Enter the last name and first name of the student who needs to pump the diary,'
-        ' as well as the subject on which you need to receive praise from the teacher.'        
-        'For example: Фролов Иван Математика',
+        'Enter the command in console: '
+        '$ python main.py schoolkid_and_subject {Фролов Иван Математика 6 А}. '
+        'Enter the last name and first name of the student, '
+        'the subject for which you want to receive praise from the teacher, '
+        'as well as the klass number and the letter in which he studies.'
+        'For example: Фролов Иван Математика 6 А',
         nargs='+',
         type=str,
         default=None,
     )
+    parser.add_argument('year',
+                        type=str,
+                        default=None)
+    parser.add_argument('group_letter',
+                        type=str,
+                        default=None)
     return parser
 
 
 def main():
+    commendations = [
+        'Молодец!',
+        'Отлично!',
+        'Хорошо!',
+        'Гораздо лучше, чем я ожидал!',
+        'Ты меня приятно удивил!',
+        'Великолепно!',
+        'Прекрасно!',
+        'Ты меня очень обрадовал!',
+        'Именно этого я давно ждал от тебя!',
+        'Сказано здорово – просто и ясно!',
+        'Ты, как всегда, точен!',
+        'Очень хороший ответ!',
+        'Талантливо!',
+        'Ты сегодня прыгнул выше головы!',
+        'Я поражен!',
+        'Уже существенно лучше!',
+        'Потрясающе!',
+        'Замечательно!',
+        'Прекрасное начало!',
+        'Так держать!',
+        'Ты на верном пути!',
+        'Здорово!',
+        'Это как раз то, что нужно!',
+        'Я тобой горжусь!',
+        'С каждым разом у тебя получается всё лучше!',
+        'Мы с тобой не зря поработали!',
+        'Я вижу, как ты стараешься!',
+        'Ты растешь над собой!',
+        'Ты многое сделал, я это вижу!',
+        'Теперь у тебя точно все получится!'
+    ]
     parser = create_parser()
     schoolkid_name = ' '.join(parser.parse_args().schoolkid_and_subject[1:3])
     subject = ' '.join(parser.parse_args().schoolkid_and_subject[3:])
-    year_of_study = 6
-    group_letter = 'А'
+    year_of_study = parser.parse_args().year
+    group_letter = parser.parse_args().group_letter
+    text = random.choice(commendations)
     try:
         schoolkid = Schoolkid.objects.get(
             full_name__contains=schoolkid_name,
@@ -86,21 +115,11 @@ def main():
             group_letter=group_letter)
         print(f'Исправлено оценок - {fix_marks(schoolkid)}')
         print(f'Удалено замечаний - {remove_chastisements(schoolkid)}')
-        commendation = create_commendation(schoolkid, subject, random.choice(commendations))
-        print(
-            f'Похвала от учителя: {commendation.text}\n'
-            f'Учитель: {commendation.teacher}\n'
-            f'Предмет: {subject}\n'
-            f'Дата похвалы от учителя: {commendation.created}'
-        )
+        print(f'Добавлена похвала от учителя - {(create_commendation(schoolkid, subject, text)).text}')
     except Schoolkid.DoesNotExist:
         print(f'Не нашёл ученика {schoolkid_name} в классе {year_of_study}{group_letter}')
     except Schoolkid.MultipleObjectsReturned:
         print(f'Несколько учеников {schoolkid_name} в классе {year_of_study}{group_letter}')
-    except Subject.DoesNotExist:
-        print(f'Предмет  {subject} в классе {year_of_study} не найден')
-    except Lesson.DoesNotExist:
-        print('Урок не найден')
 
 
 if __name__ == '__main__':
